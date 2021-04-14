4544018efeb262b5e4068561a70e9d9ec1a42f0e
skarpagavali <karpagavalli.s21@gmail.com>
2021-04-14 22:24:52 +0530

    task

---
+++ src/main/java/git/Final.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Final {
+	private void method1() {
+		System.out.println("method1");
+	}
+	private void method2() {
+		System.out.println("method2");
+	}
+	private void method3() {
+		System.out.println("method3");
+	}
+}
